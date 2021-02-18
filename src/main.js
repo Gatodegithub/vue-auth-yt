7d44239aad2db8fc616052a857dd17de0479d96c
leandro <aldoleandrosaavedramorales@gmail.com>
2021-02-18 16:15:42 -0300

    fin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhArewFGiygi4RcpGXwz2qVPlvL4vXQKI",
+  authDomain: "vue-auth-yt-92b18.firebaseapp.com",
+  projectId: "vue-auth-yt-92b18",
+  storageBucket: "vue-auth-yt-92b18.appspot.com",
+  messagingSenderId: "117804613916",
+  appId: "1:117804613916:web:a42da14d028ec29321fad9"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 createApp(App).use(router).mount('#app')
